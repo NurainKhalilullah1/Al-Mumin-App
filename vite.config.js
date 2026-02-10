95181e95edb39ae97834f850ed4222969ea83d0f
NurainKhalilullah1 <temitopenurain9@gmail.com>
2026-02-10 09:59:15 +0100

    Fix error

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,7 @@ export default defineConfig({
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom', '@supabase/supabase-js'],
-          ui: ['lucide-react', 'aos']
-        }
+
       }
     }
   }
